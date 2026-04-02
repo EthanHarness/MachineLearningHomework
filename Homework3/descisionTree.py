7b05146c221b6d3d1cee8870a1b615ed8c1e3202
Ethan Harness <ethanmichaelharness@gmail.com>
2026-04-02 12:01:29 -0500

    Strongly typed code.

--- Homework3/descisionTree.py
+++ Homework3/descisionTree.py
@@ -1,42 +1,43 @@
+from __future__ import annotations
 from typing import List
 import math
 
-CAP_SHAPE_DOMAIN = ['b','c','x','f','k','s']
-CAP_SURFACE_DOMAIN = ['f','g','y','s']
-CAP_COLOR_DOMAIN = ['n','b','c','g','r','p','u','e','w','y']
-BRUISES_DOMAIN = ['t','f']
-ODOR_DOMAIN = ['a','l','c','y','f','m','n','p','s']
-GILL_ATTCH_DOMAIN = ['a','f','d','n']
-GILL_SPACE_DOMAIN = ['c','w','d']
-GILL_SIZE_DOMAIN = ['b','n']
-GILL_COLOR_DOMAIN = ['k','n','b','h','g','r','o','p','u','e','w','y']
-STALK_SHAPE_DOMAIN = ['e','t']
-STALK_ROOT_DOMAIN = ['b','c','u','e','z','r', 'm'] 
-STALK_SURFACE_ABOVE_DOMAIN = ['f','y','k','s']
-STALK_SURFACE_BELOW_DOMAIN = ['f','y','k','s']
-STALK_COLOR_ABOVE_DOMAIN = ['n','b','c','g','o','p','e','w','y']
-STALK_COLOR_BELOW_DOMAIN = ['n','b','c','g','o','p','e','w','y']
-VEIL_TYPE_DOMAIN = ['p','u']
-VEIL_COLOR_DOMAIN = ['n','o','w','y']
-RING_NUMBER_DOMAIN = ['n','o','t']
-RING_TYPE_DOMAIN = ['c','e','f','l','n','p','s','z',]
-SPORE_PRIINT_DOMAIN = ['k','n','b','h','r','o','u','w','y']
-POPULATION_DOMAIN = ['a','c','n','s','v','y']
-HABITAT_DOMAIN = ['g','l','m','p','u','w','d']
+CAP_SHAPE_DOMAIN: List[str] = ['b','c','x','f','k','s']
+CAP_SURFACE_DOMAIN: List[str] = ['f','g','y','s']
+CAP_COLOR_DOMAIN: List[str] = ['n','b','c','g','r','p','u','e','w','y']
+BRUISES_DOMAIN: List[str] = ['t','f']
+ODOR_DOMAIN: List[str] = ['a','l','c','y','f','m','n','p','s']
+GILL_ATTCH_DOMAIN: List[str] = ['a','f','d','n']
+GILL_SPACE_DOMAIN: List[str] = ['c','w','d']
+GILL_SIZE_DOMAIN: List[str] = ['b','n']
+GILL_COLOR_DOMAIN: List[str] = ['k','n','b','h','g','r','o','p','u','e','w','y']
+STALK_SHAPE_DOMAIN: List[str] = ['e','t']
+STALK_ROOT_DOMAIN: List[str] = ['b','c','u','e','z','r', 'm'] 
+STALK_SURFACE_ABOVE_DOMAIN: List[str] = ['f','y','k','s']
+STALK_SURFACE_BELOW_DOMAIN: List[str] = ['f','y','k','s']
+STALK_COLOR_ABOVE_DOMAIN: List[str] = ['n','b','c','g','o','p','e','w','y']
+STALK_COLOR_BELOW_DOMAIN: List[str] = ['n','b','c','g','o','p','e','w','y']
+VEIL_TYPE_DOMAIN: List[str] = ['p','u']
+VEIL_COLOR_DOMAIN: List[str] = ['n','o','w','y']
+RING_NUMBER_DOMAIN: List[str] = ['n','o','t']
+RING_TYPE_DOMAIN: List[str] = ['c','e','f','l','n','p','s','z',]
+SPORE_PRIINT_DOMAIN: List[str] = ['k','n','b','h','r','o','u','w','y']
+POPULATION_DOMAIN: List[str] = ['a','c','n','s','v','y']
+HABITAT_DOMAIN: List[str] = ['g','l','m','p','u','w','d']
 
-LABEL_DOMAIN = ['p','e']
-NUMBER_OF_FEATURES = 22
-LOG_BASE = 10
+LABEL_DOMAIN: List[str] = ['p','e']
+NUMBER_OF_FEATURES: int = 22
+LOG_BASE: int = 10
 
 
 class Attribute:
-    def __init__(self, featureNumber: int, label: str = "Top"):
-        self.attributeToSplitOn = featureNumber
-        self.label = label
+    def __init__(self, featureNumber: int, label: str = "Top") -> None:
+        self.attributeToSplitOn: int = featureNumber
+        self.label: str = label
         self.childrenAttributes: List[Attribute] = []
 
     @staticmethod
-    def getDomainOfFeature(featureNumber: int):
+    def getDomainOfFeature(featureNumber: int) -> List[str]:
         match featureNumber:
             case 0: return CAP_SHAPE_DOMAIN
             case 1: return CAP_SURFACE_DOMAIN
@@ -61,39 +62,39 @@ class Attribute:
             case 20: return POPULATION_DOMAIN
             case 21: return HABITAT_DOMAIN
 
-    def addAttributeSplitToChildren(self, attr):
+    def addAttributeSplitToChildren(self, attr: Attribute) -> None:
         self.childrenAttributes.append(attr)
 
-    def printAttributeTree(self, numTabs: int = 0):
-        printVal = self.attributeToSplitOn
+    def printAttributeTree(self, numTabs: int = 0) -> str:
+        printVal: str = str(self.attributeToSplitOn)
         if self.attributeToSplitOn == -1: printVal = 'p'
         elif self.attributeToSplitOn == -2: printVal = 'e'
 
-        tabStringP = "\t" * numTabs
-        resString = tabStringP + self.label + " " + str(printVal) + "\n"
+        tabStringP: str = "\t" * numTabs
+        resString: str = tabStringP + self.label + " " + printVal + "\n"
         for x in self.childrenAttributes:
             resString += x.printAttributeTree(numTabs+1)
         return resString
     
-    def addClassLabel(self, label: str):
+    def addClassLabel(self, label: str) -> None:
         self.label = label
 
 class DescisionTree:
     @staticmethod
-    def createSplits(label: List[str], featureList: List[List[str]]):
+    def createSplits(label: List[str], featureList: List[List[str]]) -> Attribute:
         if len(label) == 1 or DescisionTree.labelsHave1Val(label): return Attribute(-1 if label[0] == 'p' else -2, label[0])
 
-        splitAttr = DescisionTree.findAttributeToSplitOn(label, featureList)
-        splitDomain = Attribute.getDomainOfFeature(splitAttr)
-        attr = Attribute(splitAttr)
+        splitAttr: int = DescisionTree.findAttributeToSplitOn(label, featureList)
+        splitDomain: List[str] = Attribute.getDomainOfFeature(splitAttr)
+        attr: Attribute = Attribute(splitAttr)
 
-        divisions = {x:[[],[]] for x in splitDomain}
+        divisions: dict[str, tuple[List[str], List[List[str]]]] = {x:([], []) for x in splitDomain}
         for l, fList in zip(label, featureList):
             divisions[fList[splitAttr]][0].append(l) 
             divisions[fList[splitAttr]][1].append(fList)
         
         for key in divisions.keys():
-            if len(divisions[key][0]) == 0: continue
+            if len(divisions[key][0]) == 0: continue #Skips over any unused keys in domain of feature
             attrChild = DescisionTree.createSplits(divisions[key][0], divisions[key][1])
             attr.addAttributeSplitToChildren(attrChild)
             attrChild.addClassLabel(key)
@@ -102,54 +103,53 @@ class DescisionTree:
             
 
     @staticmethod
-    def entropyPoisonous(labels: List[str]):
-        poisonSamples = len([x for x in labels if x == 'p'])
-        samples = len(labels)
+    def entropyPoisonous(labels: List[str]) -> float:
+        poisonSamples: int = len([x for x in labels if x == 'p'])
+        samples: int = len(labels)
         if (poisonSamples == 0 or poisonSamples == samples): return 0
 
-        p = poisonSamples / samples
-        q = 1 - p
+        p: float = poisonSamples / samples
+        q: float = 1 - p
 
         return -1*((p*math.log(p,LOG_BASE)) + (q*math.log(q,LOG_BASE)))
     
     @staticmethod
-    def entropyGivenAttribute(labels: List[str], featureList: List[List[str]], featureNumber: int):
-        domain = Attribute.getDomainOfFeature(featureNumber)
-        entropy = 0
-        samples = len(labels)
+    def entropyGivenAttribute(labels: List[str], featureList: List[List[str]], featureNumber: int) -> float:
+        domain: List[str] = Attribute.getDomainOfFeature(featureNumber)
+        entropy: float = 0.0
+        samples: int = len(labels)
 
         for attribute in domain:
-            attrCount = len([1 for x in featureList if x[featureNumber] == attribute])
+            attrCount: int = len([1 for x in featureList if x[featureNumber] == attribute])
 
             if attrCount == 0: continue
 
-            pXeqlx = attrCount / samples
-            cummulativeSum = 0
+            pXeqlx: float = attrCount / samples
+            cummulativeSum: float = 0
             for label in LABEL_DOMAIN:
-                condCount = len([1 for labelVal,features in zip(labels, featureList) if labelVal == label and features[featureNumber] == attribute])
+                condCount: int = len([1 for labelVal,features in zip(labels, featureList) if labelVal == label and features[featureNumber] == attribute])
 
                 if condCount == 0: continue
 
-                pYGivenX = condCount/attrCount
-                logpYGivenX = math.log(pYGivenX, LOG_BASE)
+                pYGivenX: float = condCount/attrCount
+                logpYGivenX: float = math.log(pYGivenX, LOG_BASE)
                 cummulativeSum += (pYGivenX*logpYGivenX)
             entropy += cummulativeSum*pXeqlx
 
         return entropy*-1
     
     @staticmethod
-    def findInformationGainForAttribute(labels: List[str], featureList: List[List[str]], featureNumber: int):
-        condEntropy = DescisionTree.entropyGivenAttribute(labels, featureList, featureNumber)
-        entropyPoisonous = DescisionTree.entropyPoisonous(labels)
+    def findInformationGainForAttribute(labels: List[str], featureList: List[List[str]], featureNumber: int) -> float:
+        condEntropy: float = DescisionTree.entropyGivenAttribute(labels, featureList, featureNumber)
+        entropyPoisonous: float = DescisionTree.entropyPoisonous(labels)
         return entropyPoisonous - condEntropy
     
     @staticmethod
-    def findAttributeToSplitOn(labels: List[str], featureList: List[List[str]]):
-        
-        maxInformationGain = 0
-        featureToSplitOn = 0
+    def findAttributeToSplitOn(labels: List[str], featureList: List[List[str]]) -> int:
+        maxInformationGain: float = 0
+        featureToSplitOn: int = 0
         for x in range(NUMBER_OF_FEATURES):
-            ig = DescisionTree.findInformationGainForAttribute(labels, featureList, x)
+            ig: float = DescisionTree.findInformationGainForAttribute(labels, featureList, x)
             if ig >= maxInformationGain:
                 maxInformationGain = ig
                 featureToSplitOn = x
@@ -161,22 +161,22 @@ class DescisionTree:
         return len(set(labels)) == 1
     
     @staticmethod
-    def makeInferenceOfFeature(rootAttribute: Attribute, featureList: List[str]):
+    def makeInferenceOfFeature(rootAttribute: Attribute, featureList: List[str]) -> str:
         if len(rootAttribute.childrenAttributes) == 0: 
             return 'p' if rootAttribute.attributeToSplitOn == -1 else 'e'
         
-        inputFeatureLabel = featureList[rootAttribute.attributeToSplitOn]
-        newRoot = None
+        inputFeatureLabel: int = featureList[rootAttribute.attributeToSplitOn]
+        newRoot: Attribute|None = None
         for childFeatureAttribute in rootAttribute.childrenAttributes:
             if inputFeatureLabel == childFeatureAttribute.label:
                 newRoot = childFeatureAttribute
                 break
         
-        assert newRoot != None, "Failed to find matching label in children" #Shouldn't happen if domains are correct
+        assert newRoot != None, "Failed to find matching label in children" #Shouldn't happen if evertying is correct
         return DescisionTree.makeInferenceOfFeature(newRoot, featureList)
 
-
-def makeAssertionOnInput(featureList: List[str]):
+#Helper to make it easier to reason about assumptions made in code
+def makeAssertionOnInput(featureList: List[str]) -> None:
     for index, x in enumerate(featureList):
         assert x in Attribute.getDomainOfFeature(index), f"Feature List, {featureList}, has domain issue for feature {index}"
     return
@@ -206,10 +206,10 @@ def getInputData(testPath: str="", trainPath: str="") -> tuple[List[tuple[str, L
 
     return (trainData, testData)
 
-def computeAccuracy(dTreeRoot, dataset, labels):
-    total = len(labels)
-    correct = 0
-    for featureList, label in zip(dataset, labels):
+def computeAccuracy(dTreeRoot: Attribute, dataPairs: List[tuple[str, List[str]]]) -> float:
+    total: int = len(dataPairs)
+    correct: int = 0
+    for label, featureList in dataPairs:
         dTreeResult = DescisionTree.makeInferenceOfFeature(dTreeRoot, featureList)
         if dTreeResult == label: correct += 1
 
@@ -222,13 +222,13 @@ def splitOut(input: List[tuple[str, List[str]]]) -> tuple[List[str], List[List[s
 
 def main():
     trainData, testData = getInputData()
-    trLabels, trFeatures = splitOut(trainData)
-    tsLabels, tsFeatures = splitOut(testData)
+    trLabels: List[str] = [x[0] for x in trainData]
+    trFeatures: List[List[str]] = [x[1] for x in trainData]
 
     attr = DescisionTree.createSplits(trLabels, trFeatures) 
     
-    trAc = computeAccuracy(attr, trFeatures, trLabels)
-    tsAc = computeAccuracy(attr, tsFeatures, tsLabels)
+    trAc = computeAccuracy(attr, trainData)
+    tsAc = computeAccuracy(attr, testData)
 
     print(f"Training Accuracy {trAc}")
     print(f"Testing Accuracy {tsAc}")
